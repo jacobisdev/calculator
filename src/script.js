593d8bfe911cf6397971e01767e274ee7fed5ae3
jacobisdev <jacobisdev@gmail.com>
2026-02-12 16:57:27 -0400

    Add functionality, calculator doesnt work properly though

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-const display = document.querySelector('.calc-display')
+const display = document.querySelector('.display');
+const btns = document.querySelectorAll('.btn');
 
 const add = (a, b) => {
     return a + b;
@@ -23,23 +24,75 @@ const operate = (a, op, b) => {
         case '-': 
             return subtract(a, b);
         case '*':
-        case 'x':
+        case '×':
             return multiply(a, b);
-        case '/': 
+        case '/':
+        case '÷':
             return divide(a, b);
         default:
             return 'Invalid Operation'
     }
 }
 
-let operation = {
-    a: 0,
-    op: '',
-    b: 0,
-}
+let total = 0, value = 0;
+let operator = '';
 
 const clear = () => {
-    operation.a = 0;
-    operation.op = '';
-    operation.b = 0;
+    total = 0;
+    operator = '';
+    value = 0;
 }
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const target = e.target;
+        const input = btn.textContent;
+
+        const targetIsNum = target.classList.contains('num') && (+input || input === '0');
+        const targetIsOp = target.classList.contains('operator');
+        const targetIsEqual = target.classList.contains('equal');
+        const targetIsClear = target.classList.contains('clear');
+        const targetIsSymbol = target.classList.contains('symbol');
+
+        if (targetIsNum) {
+            if (
+                display.textContent[0] === '0' ||
+                display.textContent === 'Invalid Operation'
+            ) display.textContent = '';
+
+            display.textContent += input;
+        }
+
+        if (targetIsOp) {
+            operator = input;
+            if (total === 0) {
+                console.log('case 1')
+                total = +display.textContent;
+            } else if (total !== 0) {
+                value = +display.textContent;
+                total = operate(total, operator, value);
+                display.textContent = total;
+            }
+            console.log(total, operator, value);
+        }
+
+        if (targetIsEqual) {
+            total = value;
+            value = +display.textContent;
+            display.textContent = operate(total, operator, value);
+            console.log(value, operator, total);
+            console.log(operate(total, operator, value));
+            clear();
+        }
+
+        if (targetIsClear) {
+            display.textContent = '0';
+            clear();
+        }
+
+        if (targetIsSymbol) {
+
+        }
+        
+    })
+})
